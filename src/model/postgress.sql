0ade8a76e30680a1bcc0fa965771172af2b644d5
MokhirbekOfficial <77888062+MokhirbekOfficial@users.noreply.github.com>
2022-02-12 23:02:46 +0500

    Tabling is started

---
+++ src/model/postgress.sql
@@ -0,0 +1,30 @@
+CREATE TABLE blog_admin (
+    admin_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    admin_name varchar(255) NOT NULL UNIQUE,
+    admin_password text NOT NULL,
+    is_super boolean DEFAULT false
+);
+
+CREATE TABLE aplications (
+    aplication_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    aplicant_name varchar(255) NOT NULL,
+    aplicant_tel bigint NOT NULL,
+    coures_type varchar(255) NOT NULL,
+    post_time timestamptz DEFAULT CURRENT_TIMESTAMP,
+    post_date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE posts (
+    post_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    post_title text NOT NULL,
+    post_time timestamptz DEFAULT CURRENT_TIMESTAMP,
+    post_date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE teachers (
+    teacher_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    teacher_name varchar(255) NOT NULL,
+    teacher_description text NOT NULL,
+    teacher_img text NOT NULL
+);
+
