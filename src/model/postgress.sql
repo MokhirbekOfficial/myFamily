50730e6bfff762a15c7064fd60f93d12571b521c
MokhirbekOfficial <77888062+MokhirbekOfficial@users.noreply.github.com>
2022-02-13 23:10:46 +0500

    Creating table is finished

--- src/model/postgress.sql
+++ src/model/postgress.sql
@@ -21,10 +21,36 @@ CREATE TABLE posts (
     post_date DATE DEFAULT CURRENT_DATE
 );
 
+CREATE TABLE tel (
+    tel_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    tel_content bigint NOT NULL
+);
+
 CREATE TABLE teachers (
     teacher_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
     teacher_name varchar(255) NOT NULL,
     teacher_description text NOT NULL,
     teacher_img text NOT NULL
 );
+
+CREATE TABLE courses (
+    course_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    course_name varchar(255) NOT NULL,
+    course_description text NOT NULL,
+    course_duration varchar(255) NOT NULL,
+    course_img text NOT NULL
+);
+
+CREATE TABLE course_teacher_connection (
+    course_teacher_connection_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    course_teacher_id uuid NOT NULL,
+    course_teacher_course_id uuid NOT NULL,
+    FOREIGN KEY (course_teacher_id)
+        REFERENCES teachers(teacher_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (course_teacher_course_id)
+        REFERENCES courses(course_id)
+        ON DELETE CASCADE
+);
+
 
